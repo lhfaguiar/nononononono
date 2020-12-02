f41ab6b6205ef36cc1b74ecf05a8497caf6ac805
lhfaguiar <lais.harumi.f@gmail.com>
2020-12-02 14:55:56 -0300

    comeca a fazer carta e tabuleiro

--- coisas/tbuleiro.py
+++ coisas/tbuleiro.py
@@ -1,27 +1,27 @@
 import pygame as pg
 
- 
-class Carta(pg.sprite.Sprite):
+
+class Tabuleiro(pg.sprite.Sprite):
     #This class represents a car. It derives from the "Sprite" class in pg.
  
-    def __init__(self, cor, classe, numero):
+    def __init__(self, color, width, height, speed):
         # Call the parent class (Sprite) constructor
         super().__init__()
  
         # Pass in the color of the car, and its x and y position, width and height.
         # Set the background color and set it to be transparent
         self.image = pg.Surface([width, height])
-        # self.image.fill(WHITE)
-        # self.image.set_colorkey(WHITE)
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
  
         #Initialise attributes of the car.
-        self.cor=cor
-        self.classe=classe
-        self.numero = numero
+        self.width=width
+        self.height=height
+        self.color = color
+        self.speed = speed
  
         # Draw the car (a rectangle!)
-
-        pg.draw.rect(self.image, self.cor, [0, 0, self.width, self.height])
+        pg.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
  
         # Instead we could load a proper picture of a car...
         # self.image = pg.image.load("car.png").convert_alpha()
@@ -29,22 +29,22 @@ class Carta(pg.sprite.Sprite):
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
  
-    def ataca(self, pixels):
+    def moveRight(self, pixels):
         self.rect.x += pixels
  
-    def defende(self, pixels):
+    def moveLeft(self, pixels):
         self.rect.x -= pixels
  
-    def armadilha(self, speed):
+    def moveForward(self, speed):
         self.rect.y += self.speed * speed / 20
  
-    def magia(self, speed):
+    def moveBackward(self, speed):
         self.rect.y -= self.speed * speed / 20
  
-    def cenario(self, speed):
+    def changeSpeed(self, speed):
         self.speed = speed
  
-    def arma(self, color):
+    def repaint(self, color):
         self.color = color
         pg.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
         
