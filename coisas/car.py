9114f8da309920b33d556dc98c2a25b2d896b12f
lhfaguiar <lais.harumi.f@gmail.com>
2020-12-02 11:09:10 -0300

    vixi, tutorial

--- coisas/car.py
+++ coisas/car.py
@@ -5,34 +5,47 @@ WHITE = (255, 255, 255)
  
 class Car(pg.sprite.Sprite):
     #This class represents a car. It derives from the "Sprite" class in pg.
-    
-    def __init__(self, color, width, height):
+ 
+    def __init__(self, color, width, height, speed):
         # Call the parent class (Sprite) constructor
         super().__init__()
-        
+ 
         # Pass in the color of the car, and its x and y position, width and height.
         # Set the background color and set it to be transparent
         self.image = pg.Surface([width, height])
         self.image.fill(WHITE)
         self.image.set_colorkey(WHITE)
  
+        #Initialise attributes of the car.
+        self.width=width
+        self.height=height
+        self.color = color
+        self.speed = speed
+ 
         # Draw the car (a rectangle!)
-        pg.draw.rect(self.image, color, [0, 0, width, height])
-        
-        # Instead we could load a proper pciture of a car...
+        pg.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+ 
+        # Instead we could load a proper picture of a car...
         # self.image = pg.image.load("car.png").convert_alpha()
  
         # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
-
+ 
     def moveRight(self, pixels):
         self.rect.x += pixels
  
     def moveLeft(self, pixels):
         self.rect.x -= pixels
-    
-    def moveUp(self, pixels):
-        self.rect.y -= pixels
-    
-    def moveDown(self, pixels):
-        self.rect.y += pixels
+ 
+    def moveForward(self, speed):
+        self.rect.y += self.speed * speed / 20
+ 
+    def moveBackward(self, speed):
+        self.rect.y -= self.speed * speed / 20
+ 
+    def changeSpeed(self, speed):
+        self.speed = speed
+ 
+    def repaint(self, color):
+        self.color = color
+        pg.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import random
 from coisas.car import Car
 
 pg.init()
- 
+
+
 GREEN = (20, 255, 140)
 GREY = (210, 210 ,210)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
-        
-SCREENWIDTH=400
-SCREENHEIGHT=500
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (100, 100, 255)
+ 
+speed = 1
+colorList = (RED, GREEN, PURPLE, YELLOW, CYAN, BLUE)
+ 
+ 
+SCREENWIDTH=800
+SCREENHEIGHT=600
  
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pg.display.set_mode(size)
@@ -22,12 +30,41 @@ pg.display.set_caption("Car Racing")
 #This will be a list that will contain all the sprites we intend to use in our game.
 all_sprites_list = pg.sprite.Group()
  
-playerCar = Car(RED, 20, 30)
-playerCar.rect.x = 200
-playerCar.rect.y = 300
+ 
+playerCar = Car(RED, 60, 80, 70)
+playerCar.rect.x = 160
+playerCar.rect.y = SCREENHEIGHT - 100
+ 
+car1 = Car(PURPLE, 60, 80, random.randint(50,100))
+car1.rect.x = 60
+car1.rect.y = -100
+ 
+car2 = Car(YELLOW, 60, 80, random.randint(50,100))
+car2.rect.x = 160
+car2.rect.y = -600
+ 
+car3 = Car(CYAN, 60, 80, random.randint(50,100))
+car3.rect.x = 260
+car3.rect.y = -300
+ 
+car4 = Car(BLUE, 60, 80, random.randint(50,100))
+car4.rect.x = 360
+car4.rect.y = -900
+ 
  
 # Add the car to the list of objects
 all_sprites_list.add(playerCar)
+all_sprites_list.add(car1)
+all_sprites_list.add(car2)
+all_sprites_list.add(car3)
+all_sprites_list.add(car4)
+ 
+all_coming_cars = pg.sprite.Group()
+all_coming_cars.add(car1)
+all_coming_cars.add(car2)
+all_coming_cars.add(car3)
+all_coming_cars.add(car4)
+ 
  
 #Allowing the user to close the window...
 carryOn = True
@@ -38,8 +75,8 @@ while carryOn:
             if event.type==pg.QUIT:
                 carryOn=False
             elif event.type==pg.KEYDOWN:
-                if event.key==pg.K_x: #Pressing the x Key will quit the game
-                     carryOn=False
+                if event.key==pg.K_x:
+                     playerCar.moveRight(10)
  
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -47,21 +84,33 @@ while carryOn:
         if keys[pg.K_RIGHT]:
             playerCar.moveRight(5)
         if keys[pg.K_UP]:
-            playerCar.moveUp(5)
+            speed += 0.05
         if keys[pg.K_DOWN]:
-            playerCar.moveDown(5)
-        
-                
+            speed -= 0.05
+ 
+ 
         #Game Logic
+        for car in all_coming_cars:
+            car.moveForward(speed)
+            if car.rect.y > SCREENHEIGHT:
+                car.changeSpeed(random.randint(50,100))
+                car.repaint(random.choice(colorList))
+                car.rect.y = -200
+ 
         all_sprites_list.update()
  
         #Drawing on Screen
         screen.fill(GREEN)
         #Draw The Road
-        pg.draw.rect(screen, GREY, [40,0, 200,300])
+        pg.draw.rect(screen, GREY, [40,0, 400,SCREENHEIGHT])
         #Draw Line painting on the road
-        pg.draw.line(screen, WHITE, [140,0],[140,300],5)
-        
+        pg.draw.line(screen, WHITE, [140,0],[140,SCREENHEIGHT],5)
+        #Draw Line painting on the road
+        pg.draw.line(screen, WHITE, [240,0],[240,SCREENHEIGHT],5)
+        #Draw Line painting on the road
+        pg.draw.line(screen, WHITE, [340,0],[340,SCREENHEIGHT],5)
+ 
+ 
         #Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
         all_sprites_list.draw(screen)
  
